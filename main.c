d0d80a03fd7e5537ab657ecacaad293f111b3780
怪叔叔 <191462660@qq.com>
2022-04-14 22:45:33 +0800

    源码

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "libavutil/avutil.h"
+#include "libavformat/avformat.h"
+int main() {
+    AVFormatContext* avFormatContext = NULL;
+    printf("Hello, World!\n");
+    return 0;
+}
